02445439b879dfaa7cbb54437b454568dc26cc0a
gunnaridi <eldaribraiv@gmail.com>
2024-10-01 23:50:15 +0500

    Add files via upload

---
+++ обтек/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("more-info-button");
+
+    button.addEventListener("click", function() {
+        window.location.href = "https://www.hollowknight.com/";
+    });
+});
